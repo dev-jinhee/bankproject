efaa6566daf30fdf84329d69584babcbeda3a93b
kdy <dev.dykang@gmail.com>
2026-09-11 14:49:27 +0900

    기본 설정

---
+++ src/main/java/kr/or/oti/bankproject/BankprojectApplication.java
@@ -0,0 +1,13 @@
+package kr.or.oti.bankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankprojectApplication.class, args);
+	}
+
+}
