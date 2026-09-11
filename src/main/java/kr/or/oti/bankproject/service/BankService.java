4f90329d4e3ed1e6d257ad831f3b33ebb940d935
KooKoo-123 <rnvlfdn1234@gmail.com>
2026-09-11 15:50:00 +0900

    BankService 5, 6, 7

--- src/main/java/kr/or/oti/bankproject/service/BankService.java
+++ src/main/java/kr/or/oti/bankproject/service/BankService.java
@@ -1,5 +1,30 @@
 package kr.or.oti.bankproject.service;
 
-public class BankService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import kr.or.oti.bankproject.domain.Account;
+import kr.or.oti.bankproject.domain.Transaction;
+
+@Service
+public class BankService {
+	
+// 5. 입금(deposit): 계좌 조회 -> 계좌에 입금 실행
+	public void deposit(String accountNo, long amount) {
+		Account account = getAccount(accountNo);
+		account.deposit(amount);
+	}
+	
+// 6. 출금(withdraw): 계좌 조회 -> 계좌에서 출금 실행(잔액 부족 시 도메인 예외 발생)
+	public void withdraw(String accountNo, long amount) {
+		Account account = getAccount(accountNo);
+		account.withdraw(amount);
+	}
+	
+// 7. 거래 내역 조회(getTransaction): 특정 계좌의 거래 내역 목록 반환
+	public List<Transaction> getTransactions(String accountNo){
+		Account account = getAccount(accountNo);
+		return account.getTransactions();
+	}
 }
