3f3c16c214764626f99aa4e1ec848fb3426ab01a
kdy <dev.dykang@gmail.com>
2026-09-11 15:18:11 +0900

    transaction domain 수정

--- src/main/java/kr/or/oti/bankproject/domain/Transaction.java
+++ src/main/java/kr/or/oti/bankproject/domain/Transaction.java
@@ -1,5 +1,42 @@
 package kr.or.oti.bankproject.domain;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transaction {
+	private LocalDate transactionDate; 
+    private LocalTime transactionTime; 
+    private String kind; //입,출금
+    private long amount;          
+    private long balance; //거래후 잔액 
+    
+    // 생성자: 거래가 발생할 때 호출하여세팅    
+    public Transaction(LocalDate transactionDate, LocalTime transactionTime, String kind, long amount, long balance) {
+        this.transactionDate = transactionDate;
+        this.transactionTime = transactionTime;
+        this.kind = kind; 
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+
+	public LocalTime getTransactionTime() {
+		return transactionTime;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public long getBalance() {
+		return balance;
+	}    
 
 }
